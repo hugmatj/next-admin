7f3b4541451d5159633ff96d110fbfec2bd8f49f
IroncladDev <connerow1115@gmail.com>
2022-01-20 06:35:06 -0600

    Update util.js

--- scripts/util.js
+++ scripts/util.js
@@ -2,7 +2,7 @@ import editJsonFile from 'edit-json-file'
 import { DIRNAME } from '../data/dirname.js'
 //get the _absolute_ **absolute** path of data.json file
 const directory = __dirname.split`/`.splice(0, __dirname.split`/`.length-4).join`/` + '/data/data.json'
-const data = editJsonFile(DIRNAME.split`/`.splice(0, DIRNAME.split`/`.length-1) + "/data.json" || process.env.JSON_PATH || directory)
+const data = editJsonFile(DIRNAME.split`/`.splice(0, DIRNAME.split`/`.length-2) + "/data/data.json" || process.env.JSON_PATH || directory)
 
 export function getJSON(){
   return data.get();
